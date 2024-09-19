fddda6825a29ef694e78aba3cd099ccc93b8d875
Kathleen Garrity <garrityk@bc.edu>
2024-09-18 22:08:55 -0400

    refactor: move instructions to own component

---
+++ src/features/rps-instructions.tsx
@@ -0,0 +1,31 @@
+import { Typography, useTheme } from "@mui/material";
+import { RPSSelectionType } from "./types";
+
+export const RPSInstructions = ({
+  userSelectedOption,
+}: {
+  userSelectedOption: RPSSelectionType | null;
+}) => {
+  const theme = useTheme();
+  return (
+    <Typography
+      variant="h3"
+      sx={{
+        textAlign: "center",
+        mb: {
+          xs: theme.spacing(4),
+          sm: theme.spacing(6),
+        },
+        fontSize: 18,
+        // fading props
+        width: userSelectedOption ? 0 : 350,
+        height: 0,
+        opacity: userSelectedOption ? 0 : 1,
+        transition: "opacity 0.5s ease, width 0.5s ease 0.5s",
+      }}
+    >
+      Click on the icon below to make your choice or type "r", "p", or "s" to
+      begin
+    </Typography>
+  );
+};
